4e2d43e48737781bc3fcd420223eed8167e2f619
unknown <abasian.arman@gmail.com>
2024-09-29 06:58:31 +0330

    add bootstrap to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  devServer: {
+    port: 5000,
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+          tagPosition: "bodyClose",
+        },
+      ],
+    },
+  },
+  css: ["~/assets//css/main.css"],
+});
